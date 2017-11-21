7c3207c178a1bef32611847371cf29650692def1
Alexei Prokudin <prokudin@jlab.org>
2017-11-21 16:03:33 -0500

    alexei

--- Python/fitlab/mcsamp.py
+++ Python/fitlab/mcsamp.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys,os
 import numpy as np
 from tools.tools import load,save,checkdir,load_config
--- Python/fitlab/resman.py
+++ Python/fitlab/resman.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys,os
 import argparse
 import numpy as np
