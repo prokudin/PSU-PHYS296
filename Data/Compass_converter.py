3f0599f2a1d7da8efeb3404b653c54d7feae0c3b
Alexey Prokudin <prokudin@jlab.org>
2018-01-15 14:55:55 -0500

    alexei

--- Data/Compass_converter.py
+++ Data/Compass_converter.py
@@ -11,13 +11,13 @@ Purpous:
 from numpy import genfromtxt as gft # Import package
 from pandas import DataFrame as df # Import package
 
-L=open('all_hplus_compass2007').readlines()     # read each line
+L=open('all_hminus_compass2007.dat').readlines()     # read each line
 L=[l.strip() for l in L]                # remove white space between values in each line
-H=L[1].replace('#PhT','PhT').split()    # save the second line as H and delete the hashtag 
-dat = gft('clas_data.dat')              # Read file from working dir as numpi array
+H=L[0].split()    # save the second line as H and delete the hashtag 
+dat = gft('all_hminus_compass2007.dat')              # Read file from working dir as numpi array
 data = df(dat, columns = H)             # save data as a panda data frame with headers
 
 # saving as csv
-data.to_csv("all_hplus_compass2007.csv",index=False)    # save as csv with out index vaues
+data.to_csv("all_hminus_compass2007.csv",index=False)    # save as csv with out index vaues
 
 
--- Python/fitlab/inputs/all.py
+++ Python/fitlab/inputs/all.py
@@ -36,24 +36,24 @@ conf['params']={}
 
 
 conf['params']['ppdf']={}
-conf['params']['ppdf']['widths0 valence']  = {'value':<<    6.76910202318145692857e-01>>,'fixed':False,'min':0,'max':10}
-conf['params']['ppdf']['widths0 sea']      = {'value':<<    1.00000000000000000000e+01>>,'fixed':False,'min':0,'max':10}
+conf['params']['ppdf']['widths0 valence']  = {'value':<<    5.23701596307329708679e-01>>,'fixed':False,'min':0,'max':10}
+conf['params']['ppdf']['widths0 sea']      = {'value':<<    0.00000000000000000000e+00>>,'fixed':False,'min':0,'max':10}
 
 
 conf['params']['pdf']={}
-conf['params']['pdf']['widths0 valence']  = {'value':<<    5.70523852569334466445e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['pdf']['widths0 sea']      = {'value':<<    6.28623603298693156560e-01>>,'fixed':False,'min':0,'max':1}
+conf['params']['pdf']['widths0 valence']  = {'value':<<    2.50000000000000000000e-01>>,'fixed':True,'min':0,'max':1}
+conf['params']['pdf']['widths0 sea']      = {'value':<<    2.50000000000000000000e-01>>,'fixed':True,'min':0,'max':1}
 
 conf['params']['ff']={}
-conf['params']['ff']['widths0 pi+ fav']   = {'value':<<    1.17131842925750184747e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['ff']['widths0 pi+ unfav'] = {'value':<<    1.36079629420065184586e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['ff']['widths0 k+ fav']    = {'value':<<    1.33700177202805969845e-01>>,'fixed':False,'min':0,'max':1}
-conf['params']['ff']['widths0 k+ unfav']  = {'value':<<    1.83440557041980034647e-01>>,'fixed':False,'min':0,'max':1}
+conf['params']['ff']['widths0 pi+ fav']   = {'value':<<    2.00000000000000011102e-01>>,'fixed':True,'min':0,'max':1}
+conf['params']['ff']['widths0 pi+ unfav'] = {'value':<<    2.00000000000000011102e-01>>,'fixed':True,'min':0,'max':1}
+conf['params']['ff']['widths0 k+ fav']    = {'value':<<    2.00000000000000011102e-01>>,'fixed':True,'min':0,'max':1}
+conf['params']['ff']['widths0 k+ unfav']  = {'value':<<    2.00000000000000011102e-01>>,'fixed':True,'min':0,'max':1}
 
 
 conf['params']['gk']={}
-conf['params']['gk']['g2']   = {'value':<<    4.28219936160267877767e-04>>,'fixed':False,'min':0,'max':10}
-conf['params']['gk']['Q0'] = {'value':<<    1.67200403317010826143e+00>>,'fixed':False,'min':1,'max':10}
+conf['params']['gk']['g2']   = {'value':<<    0.00000000000000000000e+00>>,'fixed':True,'min':0,'max':10}
+conf['params']['gk']['Q0'] = {'value':<<    1.67398382026420211588e+00>>,'fixed':True,'min':1,'max':10}
 
 ############################################################################
 # set data sets
@@ -67,28 +67,30 @@ conf['datasets']['sidis']={}
 
 conf['datasets']['sidis']['filters']={}
 conf['datasets']['sidis']['filters'][0]={}
-conf['datasets']['sidis']['filters'][0]['idx']=[1000,1001,1004,1005,1002,1003,1006,1007,2000,2001,2002]
+conf['datasets']['sidis']['filters'][0]['idx']=[2003,2004]
 #conf['datasets']['sidis']['filters'][0]['idx']=[2000,2001]
 conf['datasets']['sidis']['filters'][0]['filter']="z<0.6 and Q2>1.69 and pT>0.2 and pT<0.9"
 #conf['datasets']['sidis']['filters'][0]['filter']="z>0"
 
 conf['datasets']['sidis']['xlsx']={}
 
-conf['datasets']['sidis']['xlsx'][1000]='../database/sidis/expdata/1000.xlsx'  # |  proton   | pi+    | M_Hermes | hermes 
-conf['datasets']['sidis']['xlsx'][1001]='../database/sidis/expdata/1001.xlsx'  # |  proton   | pi-    | M_Hermes | hermes 
-conf['datasets']['sidis']['xlsx'][1004]='../database/sidis/expdata/1004.xlsx'  # |  deuteron | pi+    | M_Hermes | hermes 
-conf['datasets']['sidis']['xlsx'][1005]='../database/sidis/expdata/1005.xlsx'  # |  deuteron | pi-    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1000]='../database/sidis/expdata/1000.xlsx'  # |  proton   | pi+    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1001]='../database/sidis/expdata/1001.xlsx'  # |  proton   | pi-    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1004]='../database/sidis/expdata/1004.xlsx'  # |  deuteron | pi+    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1005]='../database/sidis/expdata/1005.xlsx'  # |  deuteron | pi-    | M_Hermes | hermes 
 
-conf['datasets']['sidis']['xlsx'][1002]='../database/sidis/expdata/1002.xlsx'  # |  proton   | k+    | M_Hermes | hermes 
-conf['datasets']['sidis']['xlsx'][1003]='../database/sidis/expdata/1003.xlsx'  # |  proton   | k-    | M_Hermes | hermes 
-conf['datasets']['sidis']['xlsx'][1006]='../database/sidis/expdata/1006.xlsx'  # |  deuteron | k+    | M_Hermes | hermes 
-conf['datasets']['sidis']['xlsx'][1007]='../database/sidis/expdata/1007.xlsx'  # |  deuteron | k-    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1002]='../database/sidis/expdata/1002.xlsx'  # |  proton   | k+    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1003]='../database/sidis/expdata/1003.xlsx'  # |  proton   | k-    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1006]='../database/sidis/expdata/1006.xlsx'  # |  deuteron | k+    | M_Hermes | hermes 
+#conf['datasets']['sidis']['xlsx'][1007]='../database/sidis/expdata/1007.xlsx'  # |  deuteron | k-    | M_Hermes | hermes 
 
     
-conf['datasets']['sidis']['xlsx'][2000]='../database/sidis/expdata/2000.xlsx'  # |  proton | pi+    | ALL | clas 
-conf['datasets']['sidis']['xlsx'][2001]='../database/sidis/expdata/2001.xlsx'  # |  proton | pi-    | ALL | clas 
+#conf['datasets']['sidis']['xlsx'][2000]='../database/sidis/expdata/2000.xlsx'  # |  proton | pi+    | ALL | clas 
+#conf['datasets']['sidis']['xlsx'][2001]='../database/sidis/expdata/2001.xlsx'  # |  proton | pi-    | ALL | clas 
 #conf['datasets']['sidis']['xlsx'][2002]='../database/sidis/expdata/2002.xlsx'  # |  proton | pi0    | ALL | clas 
     
+conf['datasets']['sidis']['xlsx'][2003]='../database/sidis/expdata/2003.xlsx'  # |  proton | pi+    | ALL | compass 
+conf['datasets']['sidis']['xlsx'][2004]='../database/sidis/expdata/2004.xlsx'  # |  proton | pi-    | ALL | compass 
     
 conf['datasets']['sidis']['norm']={}
 for k in conf['datasets']['sidis']['xlsx']: conf['datasets']['sidis']['norm'][k]={'value':1,'fixed':True,'min':0,'max':1} 
